98648051f8301bfcd3e8fdb335e92e026bc9eb17
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-08-01 21:20:38 +0000

    few changes to keymaps

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/keymap.c
@@ -7,15 +7,34 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+// custom TODO micmic precondition dactyl layout
+#define ADV MT(MOD_LCTL, KC_ESC)
+#define NAV MT(RAISE, KC_TAB)
+#define SYM MT(LOWER, KC_ENT)
+
+#define HO_S MT(MOD_LALT,KC_S)
+#define HO_T MT(MOD_LGUI,KC_T)
+#define HO_N MT(MOD_RGUI,KC_N)
+#define HO_E MT(MOD_RALT,KC_E)
+
+// interact with keyd with KC_LEAD
+
+// repeat last key
+enum custom_keycodes {
+    // it can be called REP if you want but "REPEAT" is clearer and still fits under the 8 char "limit"
+    REPEAT = SAFE_RANGE,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_DEL,
         KC_TAB , KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,                         KC_J  , KC_L  , KC_U  , KC_Y  , KC_SCLN,KC_MINS,
-        KC_LSFT, KC_A  , KC_R  , KC_S  , KC_T  , KC_G  ,                         KC_M  , KC_N  , KC_E  , KC_I  ,KC_O,KC_QUOT,
-        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_D  , KC_D  ,                         KC_K  , KC_H  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+        ADV,     KC_A  , KC_R  , HO_S  , HO_T  , KC_G  ,                         KC_M  , HO_N  , HO_E  , KC_I  ,KC_O,KC_QUOT,
+        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                         KC_K  , KC_H  , KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                          KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                         RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                         KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
+                                         NAV,KC_SPC,                             KC_BSPC, SYM,
+                                         REPEAT,KC_HOME,                         KC_END,  KC_DEL,
                                          KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
     ),
 
@@ -42,3 +61,64 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                   _______,_______,            _______,_______
     )
 };
+
+// https://gist.github.com/NotGate/3e3d8ab81300a86522b2c2549f99b131 
+// Used to extract the basic tapping keycode from a dual-role key.
+// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
+#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
+uint16_t last_keycode = KC_NO;
+uint8_t last_modifier = 0;
+
+// Initialize variables holding the bitfield
+// representation of active modifiers.
+uint8_t mod_state;
+uint8_t oneshot_mod_state;
+
+void process_repeat_key(uint16_t keycode, const keyrecord_t *record) {
+    if (keycode != REPEAT) {
+        // Early return when holding down a pure layer key
+        // to retain modifiers
+        switch (keycode) {
+            case QK_DEF_LAYER ... QK_DEF_LAYER_MAX:
+            case QK_MOMENTARY ... QK_MOMENTARY_MAX:
+            case QK_LAYER_MOD ... QK_LAYER_MOD_MAX:
+            case QK_ONE_SHOT_LAYER ... QK_ONE_SHOT_LAYER_MAX:
+            case QK_TOGGLE_LAYER ... QK_TOGGLE_LAYER_MAX:
+            case QK_TO ... QK_TO_MAX:
+            case QK_LAYER_TAP_TOGGLE ... QK_LAYER_TAP_TOGGLE_MAX:
+                return;
+        }
+        last_modifier = oneshot_mod_state > mod_state ? oneshot_mod_state : mod_state;
+        switch (keycode) {
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+                if (record->event.pressed) {
+                    last_keycode = GET_TAP_KC(keycode);
+                }
+                break;
+            default:
+                if (record->event.pressed) {
+                    last_keycode = keycode;
+                }
+                break;
+        }
+    } else { // keycode == REPEAT
+        if (record->event.pressed) {
+            register_mods(last_modifier);
+            register_code16(last_keycode);
+        } else {
+            unregister_code16(last_keycode);
+            unregister_mods(last_modifier);
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    process_repeat_key(keycode, record);
+    // It's important to update the mod variables *after* calling process_repeat_key, or else
+    // only a single modifier from the previous key is repeated (e.g. Ctrl+Shift+T then Repeat produces Shift+T)
+    mod_state = get_mods();
+    oneshot_mod_state = get_oneshot_mods();
+    return true;
+};
+
