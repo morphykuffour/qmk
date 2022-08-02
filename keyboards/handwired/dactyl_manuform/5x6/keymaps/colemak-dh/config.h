5fc59815a34b21b86fb1ca71707f08384bb6c674
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-08-02 00:39:37 +0000

    adding support for leader key

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/config.h
@@ -23,3 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MASTER_RIGHT
 
 //#define EE_HANDS
+
+
+// Leader Key
+#define LEADER_TIMEOUT 300
--- keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/colemak-dh/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                         KC_K  , KC_H  , KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                          KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
                                          NAV,KC_SPC,                             KC_BSPC, SYM,
-                                         REPEAT,KC_HOME,                         KC_END,  KC_DEL,
+                                         REPEAT,KC_LEAD,                         KC_END,  KC_DEL,
                                          KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
     ),
 
